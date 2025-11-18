04efc799939371347a60c7e54cd1d59b9475b710
juliusmarcel <95739349+juliusmarcel@users.noreply.github.com>
2025-11-19 00:11:26 +0700

    bugfix

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener("install", (e) => {
+    console.log("Service Worker Installed");
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+    console.log("Service Worker Activated");
+});
